34970e2a038bcc1ccd50780810e1fef9358dc266
Makeish <ahmedkhan_md18@vnrvjiet.in>
2020-11-02 09:52:58 +0530

    Day 54

---
+++ Codechef/Factorial.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1e9+7
+#define FastIO ios_base :: sync_with_stdio(false) ; cin.tie(NULL) ; cout.tie(NULL) ;
+
+using namespace std ;
+
+void solve()
+{
+    int n , cnt = 0 ;
+    
+    cin >> n ; 
+    
+    for(int i = 5 ; n / i > 0 ; i *= 5) {
+        cnt += n/i ;
+    }
+    
+    cout << cnt << "\n" ; 
+}
+
+int main()
+{
+    FastIO
+    
+    int testcases = 1;
+    
+    cin >> testcases ;
+    
+    while(testcases--) {
+        solve() ;
+    }
+    return 0 ; 
+}
---
+++ Codechef/Life,The Universe and Everything.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1e9+7
+#define FastIO ios_base :: sync_with_stdio(false) ; cin.tie(NULL) ; cout.tie(NULL) ;
+
+using namespace std ;
+
+void solve()
+{
+    int n ;
+    
+    while(true) {
+        
+        cin >> n ;
+        
+        if(n == 42) {
+            break ;
+        }
+        else {
+            cout << n << "\n" ; 
+        }
+    }
+}
+
+int main()
+{
+    FastIO
+    
+    int testcases = 1;
+    
+    // cin >> testcases ;
+    
+    while(testcases--) {
+        solve() ;
+    }
+    return 0 ; 
+}
