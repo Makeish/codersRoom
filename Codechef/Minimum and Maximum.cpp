e1dfad1ee980391d801237a3a283b94c44abed2b
Makeish <ahmedkhan_md18@vnrvjiet.in>
2020-12-12 17:17:18 +0530

    Day 94

---
+++ Codechef/Minimum and Maximum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1e9+7
+#define FastIO ios_base :: sync_with_stdio(false) ; cin.tie(NULL) ; cout.tie(NULL) ;
+
+using namespace std ;
+
+void solve()
+{
+    int n ; 
+    
+    cin >> n ; 
+    
+    int a[n] ;
+    
+    for(int i = 0 ; i < n ; ++i) cin >> a[i] ;
+    
+    int ans = *min_element(a , a + n) ;
+    
+    cout << 1ll * ans * (n - 1) << "\n" ;
+    
+}
+
+int main()
+{
+    FastIO
+    
+    int testcases = 1;
+    
+    cin >> testcases ;
+    
+    while(testcases--) {
+        solve() ;
+    }
+    return 0 ; 
+}
